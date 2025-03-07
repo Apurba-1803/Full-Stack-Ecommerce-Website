24dd6fe05bd477d64de6c2427e12f63a08456362
Apurba-1803 <66967283+Apurba-1803@users.noreply.github.com>
2025-03-07 21:27:11 +0530

    Update Popular.jsx

--- Full-Stack-Ecommerce-Website/frontend/src/Components/Popular/Popular.jsx
+++ Full-Stack-Ecommerce-Website/frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ export const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://full-stack-ecommerce-website-backend-d6ik.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data))
   },[])
