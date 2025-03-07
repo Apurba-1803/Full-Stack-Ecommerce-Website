13dd0abf60b9d0604441ca296c1a36867665cc7b
Apurba-1803 <66967283+Apurba-1803@users.noreply.github.com>
2025-03-07 21:27:50 +0530

    Update RelatedProducts.jsx

--- Full-Stack-Ecommerce-Website/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ Full-Stack-Ecommerce-Website/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -6,7 +6,7 @@ export const RelatedProducts = () => {
   const [popularProducts, setPopularProducts] = useState([]);
   
     useEffect(()=>{
-      fetch('http://localhost:4000/popularinwomen')
+      fetch('https://full-stack-ecommerce-website-backend-d6ik.onrender.com/popularinwomen')
       .then((response)=>response.json())
       .then((data)=>setPopularProducts(data))
     },[])
